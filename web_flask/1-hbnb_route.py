3284f40715c333dcfb92ea8e9d80aa9a74d72a03
eneje promise <enejepromise@gmail.com>
2024-07-28 15:14:49 -0700

    update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-    Starts a Flask web application
+Starts a Flask web application
 """
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello():
     """Returns a Hello"""
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
-@pp.route("/hbnb", strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Returns a hbnb"""
+    """Returns HBNB"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run()
