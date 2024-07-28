102d8bdb46af93d6e859012726a83c1b6283a960
eneje promise <enejepromise@gmail.com>
2024-07-28 15:43:55 -0700

    mandatory

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-
 """
-Starts a Flask web application:
+Starts a Flask web application
 """
-
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
-def Hello():
 
+@app.route('/', strict_slashes=False)
+def hello():
     """Returns a Hello"""
-    return "Hello HBNB"
+    return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-
-    """Returns a HBNB"""
+    """Returns HBNB"""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
